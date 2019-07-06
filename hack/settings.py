d28fb7d18a31febd4e25aa27f3286f92e7db1326
drest <drestbm@gmail.com>
2019-07-07 00:49:15 +0700

    add cors

--- hack/settings.py
+++ hack/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
 
     'main',
     'compare_app',
-
+    'corsheaders',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL=True
 
 ROOT_URLCONF = 'hack.urls'
 
